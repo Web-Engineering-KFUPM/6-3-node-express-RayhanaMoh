08e808b713e87483b70a19345c62d0c9380db9b1
RayhanaMoh <s202243580@kfupm.edu.sa>
2026-04-06 11:42:29 +0300

    Todo5

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,6 +1,7 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 const app = express();
 
 
@@ -12,7 +13,7 @@ cost PORT = 3000;
 app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
 
